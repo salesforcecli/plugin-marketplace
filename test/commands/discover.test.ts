0237924ceda63e100180b1fc1ad7f8cf32adc194
mshanemc <shane.mclaughlin@salesforce.com>
2024-04-10 11:38:23 -0500

    test: testSetup top-level

--- test/commands/discover.test.ts
+++ test/commands/discover.test.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { TestContext } from '@salesforce/core/lib/testSetup.js';
+import { TestContext } from '@salesforce/core/testSetup';
 import { expect, config } from 'chai';
 import { stubSfCommandUx } from '@salesforce/sf-plugins-core';
 import Discover from '../../src/commands/plugins/discover.js';
